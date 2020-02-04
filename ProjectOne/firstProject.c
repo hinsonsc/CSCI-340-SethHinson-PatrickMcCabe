8e69853534b168f553878243d7e73928671014c6
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-04 11:46:59 -0500

    README and opening proc file system

--- ProjectOne/firstProject.c
+++ ProjectOne/firstProject.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <dirent.h>
 /*
 Seth Hinson & Patrick McCabe
 CSCI 340 Project One, Spring 2020
@@ -11,9 +12,24 @@ struct node {
     char child[16];
     struct node *next;
 
-};
-int main(int argc, char *argv[]){
 
+};
+
+struct dirent *de;
+
+int main(int argc, char *argv[]){
+    
+    DIR *dr = opendir("/proc");
+
+     if (dr == NULL){ // opendir returns NULL if couldn't open directory  
+        printf("Could not open current directory" ); 
+        return 0; 
+    } 
+
+    while ((de = readdir(dr)) != NULL) 
+            printf("%s\n", de->d_name); 
+
+    closedir(dr);
     return 0;
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CSCI-340-SethHinson-PatrickMcCabe
  Projects repository for CSCI 340: Operating Systems
+
+ ## Project One Instructions
+ 1. Clone repository
+ 2. Change directories in the terminal to the `ProjectOne` folder
+ 3. Execute the make command or gcc equivalent
+    * `make`
+    * `gcc -g -Wall -o firstProject firstProject.c`
+4. Execute the output of the make file
+    * `./firstProject`
+5. The output will be in the terminal in the form of:
+    * (parent process id), process-name RAM
+        * (child process id), process-name RAM
