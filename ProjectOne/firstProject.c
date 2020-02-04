2ecfbf2b67770bc239c608a58537a2712cbadb3d
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-04 08:22:18 -0500

    Added SLL struct

--- ProjectOne/firstProject.c
+++ ProjectOne/firstProject.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+/*
+Seth Hinson & Patrick McCabe
+CSCI 340 Project One, Spring 2020
+Printing Proc filesystem ids
+*/
 
+struct node {
+
+    char parent[16];
+    char child[16];
+    struct node *next;
+
+};
 int main(int argc, char *argv[]){
 
     return 0;
