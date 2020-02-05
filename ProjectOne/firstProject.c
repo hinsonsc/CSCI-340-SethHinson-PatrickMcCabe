4aa2df20f920ce5d1126e8f059c840e9c9374cdb
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-02-05 00:12:23 -0500

    Work in progress

--- ProjectOne/firstProject.c
+++ ProjectOne/firstProject.c
@@ -43,13 +43,36 @@ int main(int argc, char *argv[]){
 
             //debugging print statement
             //printf("filepath: %s\n", filepath);
-
+            
+            //operations to open the file
             FILE *file;
             char buf[255];
+            printf("filepath: %s\n", filepath)
             file = fopen(filepath, "r");
+            
+            // dummy variables for parsing 
+            int tempd = 0;
+            unsigned int tempu = 0;
+            long unsigned int templu = 0;
+            long int templd = 0;
+            long long unsigned int templlu = 0;
+            char tempc;
+            //real variables
+            unsigned int PID;
+            char COMM;
+            int PPID;
+            long unsigned MEMORY;
 
-            memset(&filepath[0], 0, 32);
-            //printf("%s\n", temp);    
+            fscanf(file, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %d %d %u %u %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %d", 
+            &PID, &COMM, &tempc, &tempd, &tempd, &tempd, &tempd, &tempd, &tempu, &templu, &templu, &templu, &templu, &templu, &templu, &templd, &templd, &templd, &templd, &templd, &templd, 
+            &templlu, &MEMORY, &templd, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &tempd, &tempd, &tempu, &tempu,
+            &templlu, &templu, &templd, &templu, &templu, &templu, &templu, &templu, &templu, &templu, &tempd); 
+            
+            //current problem: parsing /proc/<PID>/stat is messed up
+            printf("tempc: %s\n", &tempc);
+            printf("PID: %d\n", PID);
+            memset(&filepath[0], 0, 32 ); //clear filepath for the next pid  
+
         }
         
 
