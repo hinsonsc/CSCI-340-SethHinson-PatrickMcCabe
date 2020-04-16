cdd230216f68e64a67db166b7ac68b5166050b00
Patrick McCabe <patrick.j.mccabe5@gmail.com>
2020-04-16 08:37:55 -0700

    Added comments

--- ProjectThree/projectThree.c
+++ ProjectThree/projectThree.c
@@ -15,10 +15,12 @@ char buff[BUFSIZE];
 
 char *bruce_buffer[5000][BUFSIZE];
 
+//declaration of semaphores
 sem_t full;
 sem_t empty;
 sem_t mutex;
 
+//max for the Q
 const int MAX = 15;
 int done = 0;
 
@@ -108,22 +110,27 @@ int Queue_Dequeue(queue_t * q,int *value) {
 
 int countWords (char buffer[]){
 
+    //used to interate over the string
     int counter = 0;
 
+    //checks for only single spaces for accuracy
     if (strlen(buffer) > 2) {
-
+        
+        //iterates over the line
         while(counter < strlen(buffer)){
             
+            //checks for special characters to signify a word
             if ((buffer[counter] == ' ' || buffer[counter] == '\n' || buffer[counter] == '\0') 
                 && (buffer[counter - 1] != ' ')){
                 
+                //protection for the counter from the threads
                 sem_wait(&mutex);
                 wordCount++;
                 sem_post(&mutex);
 
             }
 
-            
+            //move to the next array index
             counter++;
 
         }
@@ -137,18 +144,23 @@ int countWords (char buffer[]){
 
 void *consumer(void* arg){
 
+    //gets the thread id and converts to int
     int t_id = (long)arg;
 
+    //decriments empty
     sem_wait(&empty);
 
     int locate;
 
     Queue_Dequeue(Q, locate);
 
+    //used to take lines off of the Q
     char michael_buffer = bruce_buffer[locate];
 
+    //incriment full
     sem_post(&full);
 
+    //function to count words
     countWords(michael_buffer);
 
     return NULL;
@@ -161,9 +173,10 @@ void *producer(){
     //FILE *fp = fopen(file, "r"); 
 
      
-
+    //parses the files for the lines and adds them to the Q
     while(fgets(buff, BUFSIZE - 1, fp) != NULL) {
             
+            //decriments full
             sem_wait(&full);
             
             Queue_Enqueue(Q, q_num);
@@ -171,6 +184,7 @@ void *producer(){
 
             *bruce_buffer[q_num - 1] = buff;
 
+            //incriments empty
             sem_post(&empty);
 
             //printf("Buff: %s\n", buff);
@@ -190,9 +204,7 @@ void *producer(){
 
 int main(int argc, char *argv[]){
 
-    //Initialize the queue
-    
-
+    //check for the args
     if (argc != 3){
 
         printf("Usage: ./projectThree <filename or path> <number of threads>\n");
@@ -200,16 +212,21 @@ int main(int argc, char *argv[]){
 
     }
 
+    //initialize the semaphores
     sem_init(&full, 0, 15);
     sem_init(&empty, 0, 0);
     sem_init(&mutex, 0, 1);
 
+    //debugging
     printf("argc: %d\n", argc);
     printf("Argv[1]: %s\n", argv[1]);
     printf("Threads: %s\n", argv[2]);
+
+    //opening the file
     fp = NULL;
     fp = fopen(argv[1], "r");
 
+    //file pointer check
     if (fp == NULL){
 
         printf("File failed!\n");
@@ -217,25 +234,33 @@ int main(int argc, char *argv[]){
 
     }
 
+    //Initialize the queue
     Q = malloc(sizeof(node_t) * 15);
 
     Queue_Init(Q);
 
+    //number of threads to create
     int num_threads = atoi(argv[2]);
 
+    //producer and consumer threads
     pthread_t consumers_t [num_threads];
     pthread_t producer_t;
 
+    //rip off the producer thread
     assert(pthread_create(&producer_t, NULL, (void*)producer, NULL) == 0);
 
+    //array for the threads
     int thread_args[num_threads];
 
     for (int i = 0; i < argv[2]; i++){
-
+        
+        //thread ID and the creation of the consumers
         thread_args[i]= i;
         assert(pthread_create(&consumers_t[i], NULL, consumer, (void *)&thread_args[i]) == 0);
         
     }
+
+    //check on if the threads are done
 
     while(1 == 1){
 
@@ -245,7 +270,8 @@ int main(int argc, char *argv[]){
         if((done == 1) && (val <= 0)){
 
             for(int i = 0; i < num_threads; i++){
-
+                
+                //join all the threads together
                 assert(pthread_join(consumers_t[i], NULL) == 0);
 
             }
@@ -254,8 +280,7 @@ int main(int argc, char *argv[]){
 
     }
 
-    
-
+    //final wordcount
     printf("Word count: %d\n", wordCount);
 
     
